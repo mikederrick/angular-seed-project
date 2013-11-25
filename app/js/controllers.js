2cef6db8f6bd063fc8f5750e93264e7ae71c7ab9
Mike Derrick <derrickm@upmc.edu>
2013-11-25 11:00:41 -0500

    adding a simple filter poc

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,9 @@
 'use strict';
 
-function Page1Ctrl($scope) {
-  $scope.page = "Page 1";
+function Page1Ctrl($scope, $filter) {
+
+	$scope.people = [{firstName: 'Bruce', lastName: 'Wayne', birthDate: 2}, {firstName: 'Wally', lastName: 'West', birthDate: 3}, {firstName: 'Jon', lastName: 'Jones', birthDate: 1}];
+	$scope.orderedPeople = $filter('orderBy')($scope.people, 'birthDate');
 }
 
 function Page2Ctrl($scope) {
