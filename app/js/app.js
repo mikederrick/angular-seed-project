522e7a4fbc78fc30deab4c71fdbd74a732e452bb
Mike Derrick <derrickm@upmc.edu>
2014-03-05 11:38:29 -0500

    Updating the version of angular.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ngRoute']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/page1.html', controller: Page1Ctrl});
     $routeProvider.when('/view2', {templateUrl: 'partials/page2.html', controller: Page2Ctrl});
-    $routeProvider.otherwise({redirectTo: '/page1'});
+    $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
